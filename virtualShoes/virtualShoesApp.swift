a038a89a4a2f6d9587e41f662b93d24a48e553da
Miguel Monllau <59914220+Devnuvols@users.noreply.github.com>
2024-08-20 15:52:56 +0200

    Initial Commit

---
+++ virtualShoes/virtualShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtualShoesApp.swift
+//  virtualShoes
+//
+//  Created by Miguel Monllau on 20/8/24.
+//
+
+import SwiftUI
+
+@main
+struct virtualShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
